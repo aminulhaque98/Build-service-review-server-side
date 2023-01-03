6394f0b85e23459835e6c6a25bb75ac1df7d422b
aminulhaque98 <aminulhaque419@gmail.com>
2023-01-03 06:39:19 +0530

    create jwt token and 401 or 403 status create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -15,10 +16,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tg7jnth.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//jwt verify
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ massage: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('contraction').collection('services');
         const reviewCollection = client.db('contraction').collection('reviews');
+        //jwt_token create
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10d' })
+            res.send({ token })
+        })
 
         app.get('/services', async (req, res) => {
             const query = {};
@@ -36,8 +61,13 @@ async function run() {
 
         });
 
-        //reviews api
-        app.get('/reviews', async (req, res) => {
+        //reviews api with used jwt_token
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded?.email !== req?.query?.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
+
             let query = {};
             if (req.query.email) {
                 query = {
@@ -99,5 +129,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Service Review Not Server Running On Port ${port}`)
+    console.log(`Service Review Server Running On Port ${port}`)
 })
